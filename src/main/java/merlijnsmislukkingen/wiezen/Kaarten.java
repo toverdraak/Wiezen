f26999404818b59735d343987f206d604f2cf69e
Merlijn <merlijn.linde.vos@gmail.com>
2025-07-14 15:09:39 +0200

    Wat aanpassingen

--- src/main/java/merlijnsmislukkingen/wiezen/Kaarten.java
+++ src/main/java/merlijnsmislukkingen/wiezen/Kaarten.java
@@ -4,18 +4,44 @@
  */
 package merlijnsmislukkingen.wiezen;
 
+import java.util.List;
+
 /**
  *
  * @author merlijn
  */
-public class Kaarten {
+public class Kaarten implements Comparable<Kaarten>  {
     private String soort;
-    private String nummer;
-    public Kaarten(String soort, String nummer) {
+    private int nummer;
+    
+    private static final List<String> SOORT_VOLGORDE = List.of("klaveren", "ruiten", "harten", "schoppen");
+ 
+    public Kaarten(String soort, int nummer) {
         this.soort = soort;
         this.nummer = nummer;
     } 
     public String welkeKaart() {
         return soort+ "" +nummer;
+    }
+    
+    public String getSoort() {
+        return this.soort;
+    }
+    
+    public int getNummer() {
+        System.out.println(this.nummer);
+        return this.nummer;
+        
+    }
+
+    @Override
+    public int compareTo(Kaarten other) {
+        int thisSoortIndex = SOORT_VOLGORDE.indexOf(this.soort);
+        int otherSoortIndex = SOORT_VOLGORDE.indexOf(other.soort);
+
+        if (thisSoortIndex != otherSoortIndex) {
+            return Integer.compare(thisSoortIndex, otherSoortIndex);
+        }
+        return Integer.compare(this.nummer, other.nummer);
     }
 }
