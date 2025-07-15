0a13cd3a46ff26d732ac94c7c758a34268ccdc15
Merlijn <merlijn.linde.vos@gmail.com>
2025-07-15 09:47:09 +0200

    Begin bots

--- src/main/java/merlijnsmislukkingen/wiezen/Wiezen.java
+++ src/main/java/merlijnsmislukkingen/wiezen/Wiezen.java
@@ -7,13 +7,19 @@ package merlijnsmislukkingen.wiezen;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -28,43 +34,76 @@ public class Wiezen extends Application {
     private List<Kaarten> splr2 = new ArrayList<>();
     private List<Kaarten> splr3 = new ArrayList<>();
     private List<Kaarten> splr4 = new ArrayList<>();
-
+    public String troef;
     @Override
     public void start(Stage stage) throws Exception  {
         BorderPane kaarten = new BorderPane();
-        
         createDeck();
+        Kaarten troefkaart = randomCard();
+        String troefkaartsoort = troefkaart.getSoort();
+        setTroef(troefkaartsoort);
+        Kaarten.setTroef(troefkaartsoort);
+        int nrtroefkaart = troefkaart.getNummer();
+        
         Collections.shuffle(deck);
         deelKaarten();
+        Collections.sort(splr1);
         
         Group handView = new Group();
         double startAngle = -20;         // Beginrotatie
         double angleIncrement = 3.2;     // Per kaart iets meer draaien
-        double overlap = 35;             // Hoeveel kaarten overlappen
-        double startX = 200;             // Beginpositie X
+        double overlap = 55;             // Hoeveel kaarten overlappen
+        double startX = 500;             // Beginpositie X
         double y = 20;
+        
+        Group buttons = new Group();
+        double bstartX = 200;
+        double bstartY = 0;
+        
+        Button vraag = new Button("Vraag");
+        vraag.setScaleX(3);
+        vraag.setScaleY(3);
+        vraag.setLayoutX(bstartX);
+        vraag.setLayoutY(bstartY);
+        buttons.getChildren().add(vraag);
+        double bx = startX + 100;
+        Button pas = new Button("Pas");
+        pas.setLayoutX(bx);
+        pas.setLayoutY(bstartY);
+        pas.setScaleX(2);
+        pas.setScaleY(2);
+        buttons.getChildren().add(pas);
+        int midden = splr1.size() / 2;
         
         for (int i = 0; i <splr1.size(); i++) {
             Kaarten kaart =splr1.get(i);
             String soort = kaart.getSoort();
             int nr = kaart.getNummer();
             String location = "/"+ soort+nr+".png";
-            System.out.println(location);
-            Image image = new Image(Wiezen.class.getResourceAsStream(location), 80,120, true, false);
+            Image image = new Image(Wiezen.class.getResourceAsStream(location), 120,180, true, true);
             //kaarten.getChildren().add(new ImageView(image));
             ImageView kaartView = new ImageView(image);
             double angle = startAngle + i * angleIncrement;
             double x = startX + i * overlap;
+            double offsetY = y + Math.pow(i - midden, 2) * 0.8;
 
             kaartView.setLayoutX(x);
-            kaartView.setLayoutY(y);
+            kaartView.setLayoutY(offsetY);
             kaartView.setRotate(angle);
 
             handView.getChildren().add(kaartView);
         }
-        kaarten.setBottom(handView);
-
+        String location = "/"+ troef+nrtroefkaart+".png";
+        Image image = new Image(Wiezen.class.getResourceAsStream(location), 120,180, true, true);
+        ImageView kaartView = new ImageView(image);
         
+        
+        BorderPane onderkant = new BorderPane(handView);
+        onderkant.setMinHeight(200);
+        kaarten.setBottom(onderkant);
+        kaarten.setCenter(kaartView);
+        kaarten.setTop(buttons);
+        kaarten.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
         Scene kaarttafel = new Scene(kaarten, 1500, 900, Color.GREEN);
         stage.setTitle("Wiezen");
         stage.setScene(kaarttafel);
@@ -95,5 +134,16 @@ public class Wiezen extends Application {
                 splr4.add(deck.get(i));
         }
         
+    }
+    private Kaarten randomCard() {
+        Random rkaart = new Random();
+        int randomkaart = rkaart.nextInt(53);
+        return deck.get(randomkaart);        
+    }
+    public void setTroef(String soort) {
+        troef = soort;
+    }
+    public String getTroef() {
+        return troef;
     }
 }
